327efeb6746aac635be7206c5d02eab8a7de2d45
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2018-06-24 21:01:25 +0300

    IC

---
+++ src/inject/inject.js
@@ -0,0 +1,30 @@
+const App = () => {
+
+  const currentUrl = window.location.href
+
+  notOnShittyNewVersion = () => {
+    return currentUrl.includes('old.') || !document.body.innerText.includes('You can always')
+  }
+
+  const init = () => {
+    if (notOnShittyNewVersion()) {
+      return
+    }
+    window.location.href = currentUrl.replace('www.', 'old.')
+  }
+
+  return {
+    init
+  }
+}
+
+// --------------------------------------------------------------------------------------------
+
+chrome.extension.sendMessage({}, () => {
+  const readyStateCheckInterval = setInterval(() => {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval)
+      App().init()
+    }
+  }, 10)
+})
